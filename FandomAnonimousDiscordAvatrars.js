8b437ee9be1800a6103bf148aca084a56e18dabe
Antarktidov <96629481+Antarktidov@users.noreply.github.com>
2022-09-21 18:45:12 +0300

    Update FandomAnonimousDiscordAvatrars.js

--- FandomAnonimousDiscordAvatrars.js
+++ FandomAnonimousDiscordAvatrars.js
@@ -1,17 +1,20 @@
-var profileAvatar = document.querySelector('img[src="https://static.wikia.nocookie.net/663e53f7-1e79-4906-95a7-2c1df4ebbada/thumbnail/width/400/height/400"]');
-var commentsAvatars = document.querySelectorAll(".EntityHeader_avatar__5xZ8w svg");
-console.log(profileAvatar);
-console.log(commentsAvatars);
-
-discordAvatars = ['Dgray.png', 'Dgreen.jpg', 'Dblue.jpg', 'Dyellow.jpg', 'Dred.png'];
-
 function choose(choices) {
   var index = Math.floor(Math.random() * choices.length);
   return choices[index];
 }
+discordAvatars = ['Dgray.png', 'Dgreen.jpg', 'Dblue.jpg', 'Dyellow.jpg', 'Dred.png'];
 
-profileAvatar.style.background = "url(https://test.fandom.com/ru/wiki/Special:FilePath/"  + choose(discordAvatars) + ")";
-
-for (i=0;i<commentsAvatars.length;i++) {
-	commentsAvatars[i].style.background =  "url(https://test.fandom.com/ru/wiki/Special:FilePath/"  + choose(discordAvatars) + ")";
+try {
+	var profileAvatar = document.querySelector('img[src="https://static.wikia.nocookie.net/663e53f7-1e79-4906-95a7-2c1df4ebbada/thumbnail/width/400/height/400"]');
+	profileAvatar.style.background = "url(https://test.fandom.com/ru/wiki/Special:FilePath/"  + choose(discordAvatars) + ")";
+} catch {
+	console.log("Нет профиля анонима на странице");
+}
+try {
+	var commentsAvatars = document.querySelectorAll(".EntityHeader_avatar__5xZ8w svg");
+	for (i=0;i<commentsAvatars.length;i++) {
+		commentsAvatars[i].style.background =  "url(https://test.fandom.com/ru/wiki/Special:FilePath/"  + choose(discordAvatars) + ")";
+	}
+} catch {
+	console.log("Нет аватаров анонимов в комментариях");
 }
